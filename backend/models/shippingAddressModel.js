9cb1971dd587cb08aa80b9292d4c17caffe6eb31
Brendan <bmeintjes95@gmail.com>
2023-01-17 14:34:11 +0200

    created models

---
+++ backend/models/shippingAddressModel.js
@@ -0,0 +1,29 @@
+import mongoose from 'mongoose'
+
+const shippingAddressSchema = mongoose.Schema(
+  {
+    address: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+)
+
+const ShippingAddress = mongoose.model('ShippingAddress', shippingAddressSchema)
+
+export default ShippingAddress
