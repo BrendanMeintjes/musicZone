009f8d2bd88ce5878b91e5484cb6033516c03717
Brendan <bmeintjes95@gmail.com>
2023-01-17 21:46:58 +0200

    seeder created

--- backend/seeder.js
+++ backend/seeder.js
@@ -2,3 +2,11 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import users from './data/users.js'
 import products from './data/products.js'
+import User from './models/userModel.js'
+import Product from './models/Product.js'
+import Order from './models/orderModel.js'
+import connectDB from './config/db.js'
+
+dotenv.config()
+
+connectDB()
