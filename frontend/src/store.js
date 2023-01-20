06308560e2bcb7276f623a99775d256284e762a4
Brendan <bmeintjes95@gmail.com>
2023-01-20 07:31:01 +0200

    added reducers and actions

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,9 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit'
 import thunk from 'redux-thunk'
 // import { composeWithDevTools } from '@reduxjs/toolkit/dist/devtoolsExtension'
+import { productListReducer } from './reducers/productsReducers.js'
+
+const reducer = {
+  productList: productListReducer,
+}
 
 const store = configureStore({
-  reducer: {},
+  reducer,
   preloadedState: {},
   middleware: [thunk],
   devTools: process.env.NODE_ENV !== 'production',
